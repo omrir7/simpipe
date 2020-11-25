db2fde55d0b94e7312303cd45003d68ef1baf705
omrir7 <71921802+omrir7@users.noreply.github.com>
2020-11-25 18:35:38 +0200

    Add files via upload

---
+++ simpipe/src/Definitions.py
@@ -0,0 +1,99 @@
+from Support import *
+
+VERSION = "1.0"
+
+# SIMULATION_FILE = "../data/dummy.csv"
+SIMULATION_FILE = "../data/histtab_tid_0_ctr_0.csv"
+DEAFULT_TABLE_PREFIX = "histtab_tid_"
+DEFAULT_EN_ANOMALY = False
+#DEFAULT_EN_ANOMALY = False
+
+# Simulation ARGS
+NUM_THREADS = 4
+NUM_STAGES = 4
+SPECULATIVE = True  # [True, False] # True - keep push instructions without knowing the result
+ISSUE_POLICY = "RR_ANOMALY_PERSISTENT" #["RR_ANOMALY_PERSISTENT","RR"]  # ["RR", "COARSE", "EVENT","RR_ANOMALY_PERSISTENT]
+PREFETCH_POLICY = "RR_ANOMALY"  #["RR_ANOMALY","RR"]  # ["RR","RR_ANOMALY"]
+# Control args
+VERB_ON = False
+DEFAULT_TIMEOUT = 50  # Number of ticks without instruction, setting to -1 will turn it off
+# PTRMAX = 6300000
+PTRMAX = 200 # no limit
+#PTRMAX = 30
+VERB_LVL = {"NONE": 0, "NORM": 1, "DEBUG": 2}
+VERB = "DEBUG"  # [0,1,2]
+#VERB = "DEBUG"  # [0,1,2]
+
+
+def pprint(msg, verb="DEBUG"):
+    if VERB_LVL[verb] <= VERB_LVL[VERB]:
+        print(msg)
+
+
+DEFAULT_INSTRUCTION_SIZE = 4  # Instruction size in bytes
+IQ_SIZE = 8  # Instruction Queue(IQ) size
+PREFETCH_DELAY = 4  # The delay from the cycle it granted to received
+FETCH_SIZE = 4  # Default number of instructions
+
+HAZARD_MEM_DELAY = 2
+HAZARD_MULDIV_DELAY = 3
+MEM_DICT = {'mem_path': SIMULATION_FILE, 'ptrMax': None}
+
+# generate permutations
+num_thread_list = [1, 2, 4, 8]
+issue_policy_list = ["RR", "RR_ANOMALY_PERSISTENT"]  # ["RR", "COARSE", "EVENT"]
+speculative_list = [False, True]
+num_stages_list = [4, 5]
+prefetch_delay_list = [2,3,4]
+prefetch_policy_list = ["RR","RR_ANOMALY"]
+anomaly_en_list = [True,False]
+
+RGR = [["NUM_THREAD", num_thread_list], ["ISSUE_POLICY", issue_policy_list],
+       ["SPECULATIVE", speculative_list], ["NUM_STAGES", num_stages_list],
+       ["PREFETCH_DELAY", prefetch_delay_list],["PREFETCH_POLICY",prefetch_policy_list],["EN_ANOMALY",anomaly_en_list]]
+
+FAST_RGR = [["NUM_THREAD", [2]], ["ISSUE_POLICY", ["RR"]], ["SPECULATIVE", [False]],
+            ["NUM_STAGES", [4]], ["PREFETCH_DELAY", [3]]]
+
+#RGR = FAST_RGR
+REGISTER_NUM = 32
+OPCODE = {
+    "0110111": "LUI",
+    "0010111": "AUIPC",
+    "1101111": "JAL",
+    "1100111": "JALR",
+    "1100011": "BRANCH",
+    "0000011": "LOAD",
+    "0100011": "STORE",
+    "0010011": "ALUI",
+    "0110011": "ALU",
+    "0011011": "ALUIW",
+    "0111011": "ALUW",
+    "0001111": "FENCE",
+    "1110011": "ECMD",
+}
+
+BRANCH = {0: "BEQ", 1: "BNE", 4: "BLT", 5: "BGE", 6: "BLTU", 7: "BGEU"}
+LOAD = {0: "LB", 1: "LH", 2: "LW", 4: "LBU", 5: "LHU", 6: "LWU", 3: "LD"}
+STORE = {0: "SB", 1: "SH", 2: "SW", 3: "SD"}
+ALUI = {0: "ADDI", 1: "SLLI", 2: "SLTI", 3: "SLTIU", 4: "XORI", 6: "ORI", 7: "ANDI", 1: "SLLI", 5: "SRI"}
+SRI = {0: "SRLI", 1: "SRAI"}
+ALU = {0: "ADD_SUB", 1: "SLL", 2: "SLT", 3: "SLTU", 4: "XOR", 5: "SR", 6: "OR", 7: "AND"}
+ALUIW = {0: "ADDIW", 1: "SLLIW", 5: "SRIW"}
+ALUW = {0: "ADD_SUB_W", 1: "SLLW", 5: "SRW"}
+
+ADD_SUB = {0: "ADD", 1: "SUB"}
+SR = {0: "SRL", 1: "SRA"}
+
+MULDIV = {0: "MUL", 1: "MULH", 2: "MULHSU", 3: "MULHU", 4: "DIV", 5: "DIVU", 6: "REM", 7: "REMU"}
+MULDIV64 = {0: "MULW", 4: "DIVW", 5: "DIVUW", 6: "REMW", 7: "REMUW"}
+
+# Extra
+#  - Check how many instructions were flushed when using different mechanisms
+#  - Timer for end of simulation termination in case of error+end of test check
+#  - print - simulation progress every tick cycles - [YE]
+#  - Change verbosity method to level sensitive - [YE]
+#  - print to CSV pipeline, and rgr
+#  - Anomaly detection influence inside the pipeline - [David]
+
+
---
+++ simpipe/src/main.py
@@ -0,0 +1,128 @@
+import csv
+import pathlib
+import sys
+
+from Definitions import *
+
+from RegressionPermutation import *
+from RunModel import *
+from datetime import datetime
+
+
+# Running the regression list
+def run_rgr():
+    now = datetime.now()
+    res_filename = "results_"+now.strftime("%m-%d-%Y-%H_%M_%S")+".csv"
+    mem_params = mem_params_from_args()
+
+    # Extract path to where write the results of regression
+    if os.path.isfile(mem_params["mem_path"]):
+        mem_path = pathlib.Path(mem_params['mem_path'])
+        file_path = mem_path.parents[0] / res_filename
+    else:
+        file_path = pathlib.Path(mem_params['mem_path']) / res_filename
+
+    print(file_path)
+
+    csv_file = open(file_path, 'w', newline='')
+    report_writer = csv.writer(csv_file)
+
+    rgr_db = RegressionPermutation(RGR)
+    report_writer.writerow(rgr_db.key_list + ["IPC", "TICKS", "FlushedInstsWithFetchQueues","NumOfFlushes","COMMITTED_INSTS","FLUSHED_INSTS","COUNT_OF_FETCHES"])
+    for perm_list in rgr_db.perm_list_of_lists:
+        params_dict = dict()
+        params_list = list()
+        for perm in perm_list:
+            key, val = perm.split(":")
+            params_dict[key] = val
+            params_list.append(val)
+        print(params_list)
+        x = RunModel(mem_params, params_dict)
+        x.simulator()
+        params_list.append("{0:.3f}".format(x.pipeline.ipc)) #IPC
+        params_list.append(x.pipeline.last_tick) #TICKS
+        params_list.append(x.pipeline.count_flushed_inst) #FlushedInstsWithFetch
+        params_list.append(x.pipeline.execute_unit.num_of_flushes) #NumOfFlushes
+        params_list.append(x.pipeline.execute_unit.count_committed_inst) #COMMITTED_INSTS
+        params_list.append(x.pipeline.execute_unit.count_flushed_inst) #FLUSHED_INSTS
+        params_list.append(x.pipeline.total_num_of_mem_access)#COUNT_OF_FETCHES
+
+        report_writer.writerow(params_list)
+        del x, params_dict, params_list
+
+    csv_file.close()
+
+def run_single():
+    mem_params = mem_params_from_args()
+    params_dict = dict()
+    x = RunModel(mem_params,params_dict)
+    x.simulator()
+    x.report_statistics()
+
+
+def mem_params_from_args():
+    mem_params = MEM_DICT
+    if 'dir' in args_params:
+        mem_params['mem_path'] = args_params['dir']
+    if 'ptrMax' in args_params:
+        mem_params['ptrMax'] = args_params['ptrMax']
+
+    return mem_params
+
+
+def bool_arg_parsing(input_str):
+    return True if (input_str == "True" or input_str == "1") else False
+
+
+def print_help():
+    print("\nSimpipe Help Menu\n-------------------\n")
+    print("Version: "+VERSION)
+    print("<Option>=<Values>")
+    print("Option           Value                Info")
+    print("dir               str                 directory of csv tables OR csv file of memory")
+    print("single         True/False             Running simple simulation of single run of default parameters")
+    print("reg            True/False             Running regression simulation from configuration file of parameters")
+    print("verbose        True/False             Pipeline verbosity")
+
+def print_params():
+    for key,val in args_params.items():
+        print(key+"="+str(val),end=' ')
+    print()
+
+args_params = dict()
+
+if __name__ == '__main__':
+
+    args_params["single"] = False
+    args_params["reg"] = False
+    args_params["verbose"] = False
+    args_params["csv_output"] = False
+    args_params["dir"] = SIMULATION_FILE
+    args_params["ptrMax"] = PTRMAX
+
+    for arg in sys.argv[1:]:
+        if arg.startswith("dir="):
+            args_params["dir"] = arg.split("=")[1]
+        elif arg.startswith("single="):
+            args_params["single"] = bool_arg_parsing(arg.split("=")[1])
+        elif arg.startswith("reg="):
+            args_params["reg"] = bool_arg_parsing(arg.split("=")[1])
+        elif arg.startswith("--help"):
+            print_help()
+        elif arg.startswith("verbose="):
+            args_params["verbose"] = bool_arg_parsing(arg.split("=")[1])
+        elif arg.startswith("csv_output="):
+            args_params["csv_output"] = bool_arg_parsing(arg.split("=")[1])
+        elif arg.startswith("ptrMax="):
+            args_params["ptrMax"] = int(arg.split("=")[1])
+
+    print_params()
+
+    if args_params["single"]:
+        run_single()
+    elif args_params["reg"]:
+        run_rgr()
+
+
+
+
